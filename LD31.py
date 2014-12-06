8f106bfbeb93fd1fe53e6893c50c76a664aa4217
jdCarew <jacob_carew@msn.com>
2014-12-06 11:25:20 -0500

    Wheres the mountain dew? The art is mostly done. 4 Colors for now. todo: gravity, maybe improve the click algo customizablity? diff sizes, colors? set seed?

--- LD31.py
+++ LD31.py
@@ -5,27 +5,64 @@ print ("Gravity Ain't The Same")
 print ("By: jdCarew")
 print ("Ludum Dare 31")
 ##no it aint
-size = width, height= 200,200
+size = width, height= 600,400
 black=0,0,0
 square_width=40
-colors=["red2.bmp"]
-colorcount=len(colors)
-screen=pygame.display.set_mode(size)
-board=[[] for i in range(int(width/square_width))]
-for index in range(len(board)):
+images=["red2.bmp", "blue.bmp", "green.bmp", "yellow.bmp"]
+for index, col in enumerate(images):
+    images[index]=pygame.image.load(col)
+imagescount=len(images)
+maxx=int(width/square_width)
+maxy=int(height/square_width)
+board=[[] for i in range(maxx)]
+squares=[[] for i in range(maxx)]
+for r in range(maxx):
     for c in range(int(height/square_width)):
-        board[index].append(random.randint(1,colorcount))#0 reserve for empty
-red=pygame.image.load("red2.bmp")
-rect=pygame.Rect(0,0,square_width,square_width)
-images=[pygame.image.load(j) for j in colors]
+        board[r].append(random.randint(1,imagescount))#0 reserve for empty
+screen=pygame.display.set_mode(size)
 
+def count_similar(x,y,b):
+    result=1
+    c=b[x][y]
+    b[x][y]=-1
+    if x<maxx-1 and b[x+1][y]==c:
+        result+=count_similar(x+1,y,b)
+    if x> 0 and b[x-1][y]==c:
+        result+=count_similar(x-1,y,b)
+    if y<maxy-1 and b[x][y+1]==c:
+        result+=count_similar(x,y+1,b)
+    if y> 0 and b[x][y-1]==c:
+        result+=count_similar(x,y-1,b)
+        
+    return result
+
+def check_click(x,y):
+    if board[x][y] > 0:
+        c=board[x][y]
+        result=count_similar(x,y,board) 
+        if result>=3:
+            for i, row in enumerate(board):
+                for j, item in enumerate(row):
+                    if board[i][j]== -1:
+                        board[i][j] = 0
+        else:
+            for i, row in enumerate(board):
+                for j, item in enumerate(row):
+                    if board[i][j]==-1:
+                        board[i][j] = c 
 
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            check_click(int(pos[0]/40),int(pos[1]/40))
+            screen.fill(black)
+            
     for r, row in enumerate(board):
         for c, item in enumerate(row):
-            screen.blit(images[item-1],pygame.Rect(square_width*c,square_width*r,10,10))
-    ##screen.fill(black)
+            if item>=1:
+                screen.blit(images[item-1],pygame.Rect(square_width*r,square_width*c,10,10))
     
     pygame.display.flip()
